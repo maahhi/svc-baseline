0e66fb18704f2436ffc2e02a3e0531b899af95cf
maahh <maahhi.k@gmail.com>
2023-09-18 17:54:53 -0700

    make the log of preprocess data and debug all features so far

--- data-preprocess/CNN_eval_preprocessor.py
+++ data-preprocess/CNN_eval_preprocessor.py
@@ -5,8 +5,9 @@ import numpy as np
 import librosa
 import pyworld as pw
 from joblib import dump, load
+import inspect
 
-
+path = os.getcwd()
 def get_memory_usage():
     process = psutil.Process(os.getpid())
     return process.memory_info().rss / 1024**2  # return in MB
@@ -29,14 +30,24 @@ def extract_world_features(x, fs=16000):
 
     return f0, sp, ap
 
-
+log = []
+# add runtime data and time to the log file
+log.append(time.ctime())
 # Parameters
 dataset_path = r'C:\Users\maahh\Downloads\Compressed\VocalSet1-2\data_by_singer'
+log.append(f'{dataset_path=}')
 sample_rate = 16000
+log.append(f'{sample_rate=}')
 n_mels = 128
+log.append(f'{n_mels=}')
 hop_length = 512
+log.append(f'{hop_length=}')
 time_steps = 216  # Number of time-steps per spectrogram (e.g., 3 seconds at 22050 Hz with a hop_length of 512)
-limit_of_lables =2
+log.append(f'{time_steps=}')
+limit_of_lables =1
+log.append(f'{limit_of_lables=}')
+
+
 
 # Load audio files and convert to mel spectrogram
 def extract_features(audio_path):
@@ -53,6 +64,8 @@ songs = []
 labels = sorted(os.listdir(dataset_path))
 print(labels)
 for label_id, label in enumerate(labels):
+    if label_id>limit_of_lables-1:
+        break
     print(label_id,label)
     class_folder = os.path.join(dataset_path, label)
     for root, _, files in os.walk(class_folder):
@@ -73,8 +86,7 @@ for label_id, label in enumerate(labels):
                 X.append(mel_db)
                 Y.append(label_id)
                 songs.append(song_name)
-    if label_id>limit_of_lables:
-        break
+    
 
 unique_strings = set(songs)
 string_to_int = {string: i for i, string in enumerate(unique_strings)}
@@ -82,15 +94,30 @@ string_to_int = {string: i for i, string in enumerate(unique_strings)}
 # Step 2: Convert the original list
 converted_list = [string_to_int[s] for s in songs]
 
-print(converted_list)  # This will be your list of integers
-print(string_to_int)   # This will be your dictionary of string to integer mappings
+log.append(converted_list)  # This will be your list of integers
+log.append(string_to_int)   # This will be your dictionary of string to integer mappings
 
 X = np.array(X)
 Y = np.array(Y)
+
+# a function search in specific directory, find all directories start with "run+number" and make a new directory with the next number
+def make_new_dir():
+    path = os.getcwd()
+    print(path)
+    dirs = os.listdir(path)
+    run_dirs = []
+    for dir in dirs:
+        if dir.startswith("run"):
+            run_dirs.append(dir)
+    run_dirs.sort()
+    new_dir = "run" + str(int(run_dirs[-1][3:]) + 1)
+    os.mkdir(new_dir)
+    return new_dir
+new_run_dir = make_new_dir()
 songs = np.array(converted_list)
-dump(X, './run3/data.joblib')
-dump(Y, './run3/labels.joblib')
-dump(songs,'./run3/labels.joblib')
+dump(X, './'+new_run_dir+'/data.joblib')
+dump(Y, './'+new_run_dir+'/labels.joblib')
+dump(songs,'./'+new_run_dir+'/songs.joblib')
 
 # Split the data into training, validation, and test sets (e.g., 80%, 10%, 10%)
 from sklearn.model_selection import train_test_split
@@ -99,21 +126,30 @@ X_temp, X_test, Y_temp, Y_test = train_test_split(X, Y, test_size=0.10, random_s
 X_train, X_val, Y_train, Y_val = train_test_split(X_temp, Y_temp, test_size=1/9, random_state=42)
 
 
-print(f"Training samples: {X_train.shape[0]}")
-print(f"Validation samples: {X_val.shape[0]}")
-print(f"Test samples: {X_test.shape[0]}")
+log.append(f"Training samples: {X_train.shape[0]}")
+log.append(f"Validation samples: {X_val.shape[0]}")
+log.append(f"Test samples: {X_test.shape[0]}")
 
-data_loaded = load('./run2/data.joblib')
-labels_loaded = load('./run2/labels.joblib')
-print(data_loaded == X)
-print(labels_loaded == Y)
+data_loaded = load('./'+new_run_dir+'/data.joblib')
+labels_loaded = load('./'+new_run_dir+'/labels.joblib')
+# function return true if 2 ndarray are equal
+log.append(np.array_equal(data_loaded, X))
+log.append(np.array_equal(labels_loaded, Y))
+log.append(Y == labels_loaded)
+log.append(Y)
+log.append(labels_loaded)
 
 memory_after = get_memory_usage()
 
-print(f"Memory used: {memory_after - memory_before} MB")
+log.append(f"Memory used: {memory_after - memory_before} MB")
 # End the clock
 end_time = time.time()
 
 # Calculate and print the elapsed time
 elapsed_time = end_time - start_time
-print(f"Time taken: {elapsed_time:.2f} seconds")
+log.append(f"Time taken: {elapsed_time:.2f} seconds")
+
+# Save the log file
+with open('./'+new_run_dir+'/log.txt', 'w') as f:
+    for item in log:
+        f.write("%s\n" % item)
